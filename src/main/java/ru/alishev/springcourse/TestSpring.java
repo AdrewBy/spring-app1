7405335b4f0fd5b17ac96ed8c80742b554a81958
Andrei Ustsinau <alchimby@gmail.com>
2023-06-11 20:36:14 +0200

    Spring.DI through setters

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -6,11 +6,23 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-   //     Music music = context.getBean("musicBean", Music.class);
-   //     MusicPlayer musicPlayer = new MusicPlayer(music);
+        //     Music music = context.getBean("musicBean", Music.class);
+        //     MusicPlayer musicPlayer = new MusicPlayer(music);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
         context.close();
+
+
+        // как работает Spring через сеттер
+        //   MusicPlayer musicPlayer1 = new MusicPlayer(); //создает bean(обьект) класса и использует пустой конструктор
+        //   musicPlayer1.setMusic(); в качестве параметра передает тот бин, который был создан ранее
+        //   <bean id="musicBean"
+        //          class="ru.alishev.springcourse.ClassicalMusic">
+        //    </bean>
+
     }
 }
