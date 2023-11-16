738a4d14d57ae89a283cc769a2f0b571586bedbe
AdrewBy <134864428+AdrewBy@users.noreply.github.com>
2023-11-16 11:18:35 +0100

    Spring.annotation component, autowired+ homework

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,38 +1,36 @@
 package ru.alishev.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.util.Random;
 
 @Component
 public class MusicPlayer {
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-    private RapMusic rapMusic;
-    @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic, RapMusic rapMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-        this.rapMusic = rapMusic;
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
+    public String getName() {
+        return name;
     }
 
+    public int getVolume() {
+        return volume;
+    }
 
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-    public void playMusic(Genres genres) {
-        Random random = new Random();
-        // случайное целое число между 0 и 2
-        int randomNumber = random.nextInt(3);
-        if(genres == Genres.CLASSICAL){
-            // случайная классическая песня
-            System.out.println(classicalMusic.getSong().get(randomNumber));
-        } else if(genres == Genres.ROCK){
-            // случайная рок песня
-            System.out.println(rockMusic.getSong().get(randomNumber));
-        }else {
-            System.out.println(rapMusic.getSong().get(randomNumber));
-        }
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
+    }
+
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
 
     }
 
